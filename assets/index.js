64e06318eadebbe98fe3fe837d6d4cac541b7a2a
xmrgd <madgadv4@gmail.com>
2025-03-29 11:28:04 +0000

    Initial commit

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 4ecc257cbb25ccc'
+            'Authorization': 'Client-ID c2b6d4cc98139de'
         },
         body: data
     })
